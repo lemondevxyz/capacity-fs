8dce7f8942d86ef8a7aef5658e9657c16dd23c9b
Ahmed Mazin <lemon@lemondev.xyz>
2022-11-22 00:08:30 +0800

    add documentation

--- file.go
+++ file.go
@@ -8,14 +8,20 @@ import (
 
 type fileSize struct {
 	afero.File
+	// hasEnoughCapacity is the function that fileSize uses to check
+	// if a file can write or not.
 	hasEnoughCapacity func(i int64) bool
-	addCapacity       func(i int64)
+	// addCapacity is a function that adds the size of the file's content.
+	addCapacity func(i int64)
 }
 
 var (
 	ErrNotEnoughCapacity = fmt.Errorf("not enough capacity")
 )
 
+// Write first checks the size of the byte of slices and returns
+// an error if it larger than the allowed size. Otherwise, it just
+// delegates the Write operation to the underlying file.
 func (f *fileSize) Write(p []byte) (n int, err error) {
 	if !f.hasEnoughCapacity(int64(len(p))) {
 		return 0, ErrNotEnoughCapacity
@@ -27,6 +33,7 @@ func (f *fileSize) Write(p []byte) (n int, err error) {
 	return n, err
 }
 
+// WriteAt is the same as Write but with an offset.
 func (f *fileSize) WriteAt(p []byte, off int64) (n int, err error) {
 	if !f.hasEnoughCapacity(int64(len(p))) {
 		return 0, ErrNotEnoughCapacity
@@ -38,6 +45,7 @@ func (f *fileSize) WriteAt(p []byte, off int64) (n int, err error) {
 	return n, err
 }
 
+// Truncate some of the file's content.
 func (f *fileSize) Truncate(size int64) error {
 	err := f.File.Truncate(size)
 	if err != nil {
--- fs.go
+++ fs.go
@@ -10,6 +10,11 @@ import (
 	"github.com/spf13/afero"
 )
 
+// CalculateSizeSum is a function that takes in a file system and walks
+// through that filesystem to get the total size of the filesystem.
+//
+// This function will only return an error if a directory has been read
+// through ReadDir but the file couldn't be Stat'd.
 func CalculateSizeSum(f afero.Fs) (int64, error) {
 	var size int64 = 0
 
@@ -31,18 +36,24 @@ type capacityFs struct {
 	mtx         sync.RWMutex
 }
 
+// hasEnoughCapacity checks whether or not size can be added or not.
 func (a *capacityFs) hasEnoughCapacity(size int64) bool {
 	a.mtx.RLock()
 	defer a.mtx.RUnlock()
 	return (size + a.cachedSize) <= a.limitedSize
 }
 
+// addCapacity adds the size to the structure's cachedSize
 func (a *capacityFs) addCapacity(size int64) {
 	a.mtx.Lock()
 	a.cachedSize += size
 	a.mtx.Unlock()
 }
 
+// statAndCheckSize returns an error if the size of a file is more than
+// the limited size.
+//
+// This is only called on Create, Mkdir, MkdirAll.
 func (a *capacityFs) statAndCheckSize(name string) error {
 	stat, err := a.Fs.Stat(name)
 	if err == nil {
@@ -58,12 +69,16 @@ func (a *capacityFs) statAndCheckSize(name string) error {
 	return nil
 }
 
+// DEPRECATED
+// deferError removes a file
 func (a *capacityFs) deferError(name string, err error) {
 	if err != nil {
 		a.Fs.Remove(name)
 	}
 }
 
+// Create creates a file but fails if the size of the file is more than
+// the limited size.
 func (a *capacityFs) Create(name string) (afero.File, error) {
 	fi, err := a.Fs.Create(name)
 	if err != nil {
@@ -72,11 +87,13 @@ func (a *capacityFs) Create(name string) (afero.File, error) {
 
 	fi, err = &fileSize{fi, a.hasEnoughCapacity, a.addCapacity}, a.statAndCheckSize(name)
 	if err != nil {
-		a.deferError(name, err)
+		a.Fs.Remove(name)
 	}
 	return fi, err
 }
 
+// Mkdir creates a directory but could fail if the directory size is
+// more than the limited size.
 func (a *capacityFs) Mkdir(name string, perm os.FileMode) error {
 	err := a.Fs.Mkdir(name, perm)
 	if err != nil {
@@ -85,11 +102,14 @@ func (a *capacityFs) Mkdir(name string, perm os.FileMode) error {
 
 	err = a.statAndCheckSize(name)
 	if err != nil {
-		a.deferError(name, err)
+		a.Fs.Remove(name)
 	}
 	return err
 }
 
+// possibleCombinations returns a slice of possible path combinations.
+//
+// E.g. ["dir1", "dir2", "dir3"] => ["dir1", "dir1/dir2", "dir1/dir2/dir3"]
 func possibleCombinations(str []string) (ret []string) {
 	for k := range str {
 		ret = append(ret, strings.Join(str[:k+1], "/"))
@@ -98,6 +118,12 @@ func possibleCombinations(str []string) (ret []string) {
 	return ret
 }
 
+// MkdirAll is a wrapper around Mkdir that creates all directories that
+// do not exist.
+//
+// i.e. /dir1/dir2/dir3/dir4
+//
+// If dir1 exists, but dir2 doesn't it creates it and its subchildren.
 func (a *capacityFs) MkdirAll(name string, perm os.FileMode) (err error) {
 	for _, v := range possibleCombinations(strings.Split(name, "/")) {
 		_, err := a.Fs.Stat(v)
@@ -114,6 +140,7 @@ func (a *capacityFs) MkdirAll(name string, perm os.FileMode) (err error) {
 	return nil
 }
 
+// Open opens a File with a size limit
 func (a *capacityFs) Open(name string) (fi afero.File, err error) {
 	fi, err = a.Fs.Open(name)
 	if err != nil {
@@ -123,6 +150,7 @@ func (a *capacityFs) Open(name string) (fi afero.File, err error) {
 	return &fileSize{fi, a.hasEnoughCapacity, a.addCapacity}, nil
 }
 
+// OpenFile opens a File with a size limit
 func (a *capacityFs) OpenFile(name string, flag int, perm os.FileMode) (afero.File, error) {
 	fi, err := a.Fs.OpenFile(name, flag, perm)
 	if err != nil {
@@ -132,6 +160,8 @@ func (a *capacityFs) OpenFile(name string, flag int, perm os.FileMode) (afero.Fi
 	return &fileSize{fi, a.hasEnoughCapacity, a.addCapacity}, nil
 }
 
+// beforeRemove essentially returns the size of the file or the real
+// size of a directory.
 func (a *capacityFs) beforeRemove(name string) (int64, error) {
 	var size int64 = 0
 	stat, err := a.Fs.Stat(name)
@@ -149,8 +179,8 @@ func (a *capacityFs) beforeRemove(name string) (int64, error) {
 	return size, nil
 }
 
-// ci comment
-
+// Remove a file in a directory and also syncs the current
+// filesystem size.
 func (a *capacityFs) Remove(name string) error {
 	size, err := a.beforeRemove(name)
 	if err != nil {
@@ -167,6 +197,8 @@ func (a *capacityFs) Remove(name string) error {
 	return nil
 }
 
+// RemoveAll removes a directory and its subchildren or a file and also
+// syncs the current filesystem size.
 func (a *capacityFs) RemoveAll(name string) error {
 	size, err := a.beforeRemove(name)
 	if err != nil {
@@ -183,10 +215,16 @@ func (a *capacityFs) RemoveAll(name string) error {
 	return nil
 }
 
+// Name modifies the name of the filesystem.
 func (a *capacityFs) Name() string {
 	return fmt.Sprintf("capacityFs %d - %s", a.limitedSize, a.Fs.Name())
 }
 
+// NewCapacityFs is a function that returns a filesystem of a specific
+// capacity.
+//
+// This function will return an error if the filesystem was above the
+// provided capacity.
 func NewCapacityFs(f afero.Fs, size int64) (afero.Fs, error) {
 	if f == nil {
 		return nil, fmt.Errorf("nil interface")
